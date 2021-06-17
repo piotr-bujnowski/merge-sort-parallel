ceeb4545c0baf93df4a9aee0cae6ddf26bb3135e
Piotr Bujnowski <bujan98pl@gmail.com>
2021-06-17 13:41:54 +0200

    Divide MergeSort into standalone class

--- Merge-Sort-Parallel/MergeSort.cs
+++ Merge-Sort-Parallel/MergeSort.cs
@@ -4,49 +4,6 @@ namespace Merge_Sort_Parallel
 {
     class MergeSort
     {
-        public static void Main(String[] args)
-        {
-            Random random = new Random();
-            string exit;
-            int numSize;
-
-            do
-            {
-                Console.Write($"\nPodaj ilość liczb do wygenerowania losowo: \n> ");
-                do
-                {
-                    while (!int.TryParse(Console.ReadLine(), out numSize))
-                    {
-                        Console.WriteLine("Podana wartość nie jest liczbą!");
-                    }
-
-                    if (numSize <= 0)
-                    {
-                        Console.WriteLine("Podana wartość nie może być <= 0!");
-                    }
-                }
-                while (numSize <= 0);
-
-                int[] arr = new int[numSize];
-                for (int i = 0; i < arr.Length; i++)
-                {
-                    arr[i] = random.Next(100);
-                }
-
-                Console.WriteLine("\nWygenerowane tablica:");
-                printArray(arr);
-
-                MergeSort ob = new MergeSort();
-                ob.sort(arr, 0, arr.Length - 1);
-
-                Console.WriteLine("\nPosortowane tablica: ");
-                printArray(arr);
-
-                Console.Write("\nCzy chcesz powtórzyć sortowanie? [y/n]: \n> ");
-                exit = Console.ReadLine();
-            } while (!exit.Equals("n"));
-        }
-
         public void merge(int[] array, int left, int middle, int right)
         {
             // Znajdz wielkość dwóch tablic do scalenia
@@ -121,15 +78,6 @@ namespace Merge_Sort_Parallel
                 // Scal posortowane tablice
                 merge(array, left, middle, right);
             }
-        }
-
-        // Drukuj tablice
-        static void printArray(int[] arr)
-        {
-            int n = arr.Length;
-            for (int i = 0; i < n; ++i)
-                Console.Write(arr[i] + " ");
-            Console.WriteLine();
         }
     }
 }
---
+++ Merge-Sort-Parallel/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Merge_Sort_Parallel
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Random random = new Random();
+            string exit;
+            int numSize;
+
+            do
+            {
+                Console.Write($"\nPodaj ilość liczb do wygenerowania losowo: \n> ");
+                do
+                {
+                    while (!int.TryParse(Console.ReadLine(), out numSize))
+                    {
+                        Console.WriteLine("Podana wartość nie jest liczbą!");
+                    }
+
+                    if (numSize <= 0)
+                    {
+                        Console.WriteLine("Podana wartość nie może być <= 0!");
+                    }
+                }
+                while (numSize <= 0);
+
+                int[] arr = new int[numSize];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = random.Next(100);
+                }
+
+                Console.WriteLine("\nWygenerowane tablica:");
+                printArray(arr);
+
+                MergeSort ob = new MergeSort();
+                ob.sort(arr, 0, arr.Length - 1);
+
+                Console.WriteLine("\nPosortowane tablica: ");
+                printArray(arr);
+
+                Console.Write("\nCzy chcesz powtórzyć sortowanie? [y/n]: \n> ");
+                exit = Console.ReadLine();
+            } while (!exit.Equals("n"));
+        }
+
+        // Drukuj tablice
+        static void printArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
+        }
+    }
+}
